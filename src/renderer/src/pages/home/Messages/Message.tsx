01436825da73aece7e0e11c9441a754870c6dd29
icarus <eurfelux@gmail.com>
2025-06-10 04:41:34 +0800

    fix: bubble editing

--- src/renderer/src/pages/home/Messages/Message.tsx
+++ src/renderer/src/pages/home/Messages/Message.tsx
@@ -156,7 +156,7 @@ const MessageItem: FC<Props> = ({
             display: 'flex',
             flexDirection: 'column',
             alignSelf: isAssistantMessage ? 'flex-start' : 'flex-end',
-            flex: isBubbleStyle ? undefined : 1
+            flex: 1
           }}>
           <MessageHeader
             message={message}
